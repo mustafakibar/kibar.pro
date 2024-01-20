9ddf2d9e194b4d984f14e4918a1a2927e7a4aa81
Mustafa KiBAR <mustafa@kibar.pro>
2024-01-20 22:01:14 +0300

    add prettier config minor tweak on eslint configuration

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSameLine: true,
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+};
