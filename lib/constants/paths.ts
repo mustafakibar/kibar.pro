fe59a51c82fcd273db65f2dc5cf750cf23d0a78b
Mustafa KiBAR <mustafa@kibar.pro>
2026-05-05 00:42:54 +0300

    refactor(paths): add CONTACT/USES/NOTES paths for v2 nav

--- lib/constants/paths.ts
+++ lib/constants/paths.ts
@@ -2,7 +2,19 @@ const HOME_PATH = '/';
 const ABOUT_PATH = '/about';
 const PROJECTS_PATH = '/projects';
 const CERTIFICATES_PATH = '/certificates';
+const CONTACT_PATH = '/contact';
+const USES_PATH = '/uses';
+const NOTES_PATH = '/notes';
 const RESUME_PATH =
   'https://drive.google.com/file/d/1JiFtl1BGmENDRV5OV3MpcaydRlGOS-2M';
 
-export { ABOUT_PATH, CERTIFICATES_PATH, HOME_PATH, PROJECTS_PATH, RESUME_PATH };
+export {
+  ABOUT_PATH,
+  CERTIFICATES_PATH,
+  CONTACT_PATH,
+  HOME_PATH,
+  NOTES_PATH,
+  PROJECTS_PATH,
+  RESUME_PATH,
+  USES_PATH,
+};
