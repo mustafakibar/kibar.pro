83bca3344709162553c23d2a60894793114b37bc
Mustafa KiBAR <mustafa@kibar.pro>
2026-05-05 00:00:20 +0300

    refactor(icons): replace react-icons with lucide-react re-exports

--- lib/icons.ts
+++ lib/icons.ts
@@ -1,29 +1,29 @@
-import { BsEnvelope } from 'react-icons/bs';
-import { FaCheck, FaChevronRight, FaHeart } from 'react-icons/fa';
-import {
-  FaArrowLeft,
-  FaArrowRight,
-  FaGithub,
-  FaGitlab,
-  FaRegCircle,
-} from 'react-icons/fa6';
-import { LuMilestone, LuSquareStack } from 'react-icons/lu';
-import { MdMenu, MdMenuOpen } from 'react-icons/md';
-import { TbChevronUpRight } from 'react-icons/tb';
-
 export {
-  FaArrowLeft as ArrowLeft,
-  FaArrowRight as ArrowRight,
-  MdMenu as BurgerMenu,
-  MdMenuOpen as BurgerMenuOpened,
-  FaCheck as Check,
-  FaChevronRight as ChevronRight,
-  TbChevronUpRight as ChevronUpRight,
-  FaRegCircle as Circle,
-  BsEnvelope as Envelope,
-  FaGithub as Github,
-  FaGitlab as Gitlab,
-  FaHeart as HeartIcon,
-  LuMilestone as Milestone,
-  LuSquareStack as SquareStack,
-};
+  GraduationCap as AcademicCapIcon,
+  ArrowLeft,
+  ArrowRight,
+  Menu as BurgerMenu,
+  X as BurgerMenuOpened,
+  Check,
+  ChevronRight,
+  MoveUpRight as ChevronUpRight,
+  CircleAlert,
+  Code as CodeIcon,
+  Command as CommandIcon,
+  Copy,
+  Mail as Envelope,
+  ExternalLink,
+  FileText,
+  Folder,
+  Github,
+  Gitlab,
+  Heart as HeartIcon,
+  Home as HomeIcon,
+  Linkedin,
+  Milestone,
+  Search,
+  Layers as SquareStack,
+  Wrench as ToolboxIcon,
+  User as UserIcon,
+  X as XIcon,
+} from 'lucide-react';
