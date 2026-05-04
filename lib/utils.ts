fc1ca4b2ff9b8efe04f8d8d64a2d8151eac8c89d
Mustafa KiBAR <mustafa@kibar.pro>
2026-05-05 00:00:35 +0300

    refactor(utils): drop getRandomInt (hydration risk; consumer being removed)

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
+const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-const getRandomInt = (min: number, max: number) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-export { cn, getRandomInt };
+export { cn };
